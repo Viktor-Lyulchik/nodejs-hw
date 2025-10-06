7a9d641c6246e24cf7bd48f780993c4ac271c843
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2025-10-07 00:19:21 +0300

    add debud mail connection

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,10 +4,19 @@ import { env } from './env.js';
 const transporter = nodemailer.createTransport({
   host: env('SMTP_HOST'),
   port: env('SMTP_PORT'),
+  secure: true,
   auth: {
     user: env('SMTP_USER'),
     pass: env('SMTP_PASSWORD'),
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('SMTP connection failed:', error);
+  } else {
+    console.log('Server is ready to send emails');
+  }
 });
 
 export const sendMail = async (options) => {
